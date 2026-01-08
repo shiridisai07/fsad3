45217247a0845351111a0d7ade4d4c3b96bef61a
chinnam shiridi sai <chinnamshiridisai@gmail.com>
2026-01-08 14:54:36 +0530

    8/1/2026

---
+++ DI-CourseRegistration-XML/src/main/java/com/klu/main/MainApp.java
@@ -0,0 +1,15 @@
+package com.klu.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.klu.model.CourseRegistration;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CourseRegistration cr =(CourseRegistration) context.getBean("CourseReg");
+
+        cr.display();
+    }
+}
