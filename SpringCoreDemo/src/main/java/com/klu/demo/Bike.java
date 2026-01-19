a4d991d1724670875307e9217448cfece380a715
chinnam shiridi sai <chinnamshiridisai@gmail.com>
2026-01-19 12:00:17 +0530

    8/1/26

---
+++ SpringCoreDemo/src/main/java/com/klu/demo/Bike.java
@@ -0,0 +1,14 @@
+package com.klu.demo;
+
+public class Bike {
+    private Engine engine;
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void ride() {
+        engine.start();
+        System.out.println("Bike is moving...");
+    }
+}
