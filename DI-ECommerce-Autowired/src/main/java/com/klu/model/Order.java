b97e299c3a01a8d000d435c2f0d0ad423d75ff87
chinnam shiridi sai <chinnamshiridisai@gmail.com>
2026-01-21 10:24:55 +0530

    21/01/2026

---
+++ DI-ECommerce-Autowired/src/main/java/com/klu/model/Order.java
@@ -0,0 +1,39 @@
+package com.klu.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Order {
+	private int orderId;
+	private String customerName;
+	private int quantity;
+	@Autowired
+	private Product product;
+	
+	public Order()
+	{
+		this.orderId=2004;
+		this.customerName="Sai";
+		this.quantity=4;
+		
+	}
+	public void display()
+	{
+		System.out.println("The following are the Oredered sequence of Customers");
+		System.out.println("OrderId:"+orderId);
+		System.out.println("Custname:"+customerName);
+		System.out.println("Quantity:"+quantity);
+		System.out.println("ProductId:"+product.getProductId());
+		System.out.println("ProductName:"+product.getProductName());
+		System.out.println("ProductPrice:"+product.getPrice());
+		System.out.println("ProductCategory:"+product.getCategory());
+		
+
+		
+		
+		
+		
+	}
+
+}
