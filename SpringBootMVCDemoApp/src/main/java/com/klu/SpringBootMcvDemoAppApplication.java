7adcf05d897e756e8e8d13b154b661210f16f56b
chinnam shiridi sai <chinnamshiridisai@gmail.com>
2026-02-11 10:30:11 +0530

    10/02.2026

---
+++ SpringBootMVCDemoApp/src/main/java/com/klu/SpringBootMcvDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMcvDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMcvDemoAppApplication.class, args);
+	}
+
+}
---
+++ SpringBootMVCDemoApp/src/main/java/com/klu/service/MessageService.java
@@ -0,0 +1,12 @@
+package com.klu.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+	public String getMessage()
+	{
+		return "Welcome to Spring Boot MVC";
+	}
+
+}
